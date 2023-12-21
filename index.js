1409ec7520ca662d6d84663b7ded0e59d4b209b2
Dennis <denkipyator@gmail.com>
2023-12-21 11:18:39 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function writeCards(array, event){
+    let messages = [];
+    
+    for (let i = 0;i < array.length; i++){
+        let message = `Thank you, ${array[i]}, for the wonderful ${event} gift!`;
+        messages.push(message);
+    }
+
+    return messages;
+}
+
+function countDown(number){
+    while(number >= 0){
+        console.log(number);
+        number--;
+    }
+}
 // Code your solutions in this file
